5dbf4ff950c2bd2a7d1f030dea643369601037de
Marcio Barros <marcio.barros@gmail.com>
2014-02-12 21:38:20 -0200

    Simplificacao do script

--- r/revisions-version.r
+++ r/revisions-version.r
@@ -1,74 +1,54 @@
-data <- read.table("/Users/Marcio/Documents/GitHub/Pesquisa/SBSE/sbse-ant-unirio/revisions.data", header=TRUE);
+data <- read.table("/Users/Marcio Barros/Documents/GitHub/sbse-ant-unirio/results/versioncontrol/log_versions.data", header=TRUE);
+
+data$version[data$version == "1.4.1"] <- "1.4.0";
+data$version[data$version == "1.5.1"] <- "1.5.0";
+data$version[data$version == "1.5.2"] <- "1.5.0";
+data$version[data$version == "1.5.3"] <- "1.5.0";
+data$version[data$version == "1.5.4"] <- "1.5.0";
+data$version[data$version == "1.6.1"] <- "1.6.0";
+data$version[data$version == "1.6.2"] <- "1.6.0";
+data$version[data$version == "1.6.3"] <- "1.6.0";
+data$version[data$version == "1.6.4"] <- "1.6.0";
+data$version[data$version == "1.6.5"] <- "1.6.0";
+data$version[data$version == "1.7.1"] <- "1.7.0";
+data$version[data$version == "1.8.1"] <- "1.8.0";
+data$version[data$version == "1.8.3"] <- "1.8.2";
+data$version[data$version == "1.8.4"] <- "1.8.2";
+data$version <- factor(data$version);
 
 versions <- rev(unique(data$version));
-rel_versions <- factor(c("1.1.0", "1.2.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0", "1.8.0", "1.8.2", "1.9.0"));
-
-columns <- c("rev", "single", "revmc", "revsc", "pv_c", "revmp", "revsp", "pv_p");
-result <- matrix(nrow=length(rel_versions), ncol=length(columns), dimnames=list(rel_versions, columns));
-
+columns <- c("rev", "single", "revmc", "revsc", "pv_c", "revmp", "revsp", "sw_p", "pv_p");
+result <- matrix(nrow=length(versions), ncol=length(columns), dimnames=list(versions, columns));
 lastVersion <- "";
-accRevisions <- 0;
-accSingleRevisions <- 0;
-oldClasses <- c();
-oldPackages <- c();
-accClasses <- c();
-accPackages <- c();
 
 for (version_ in versions)
 {
-	for (findVersion_ in rel_versions)
+	versionData <- subset(data, version == version_);
+	
+	result[version_, "rev"] <- nrow(versionData);
+	result[version_, "single"] <- nrow(subset(versionData, classes == 1)); 
+	
+	result[version_, "revmc"] <- mean(versionData$classes);
+	result[version_, "revsc"] <- sd(versionData$classes);
+	
+	result[version_, "revmp"] <- mean(versionData$packages);
+	result[version_, "revsp"] <- sd(versionData$packages);
+	
+	if (version_ != "1.1.0")
 	{
-		if (findVersion_ == version_)
-		{
-			if (lastVersion != "")
-			{
-				result[lastVersion, "rev"] <- accRevisions;
-				result[lastVersion, "single"] <- accSingleRevisions; 
-				
-				result[lastVersion, "revmc"] <- mean(accClasses);
-				result[lastVersion, "revsc"] <- sd(accClasses);
-
-				if (lastVersion != "1.1.0")
-					result[lastVersion, "pv_c"] <- wilcox.test(oldClasses, accClasses)$p.value; 
-				
-				result[lastVersion, "revmp"] <- mean(accPackages);
-				result[lastVersion, "revsp"] <- sd(accPackages);
-				
-				if (lastVersion != "1.1.0")
-					result[lastVersion, "pv_p"] <- wilcox.test(oldPackages, accPackages)$p.value; 
-			}
-			
-			lastVersion <- version_;
-			accRevisions <- 0;
-			accSingleRevisions <- 0;
-			oldClasses <- accClasses;
-			oldPackages <- accPackages;
-			accClasses <- c();
-			accPackages <- c();
-		}
+		lastVersionData <- subset(data, version == lastVersion);
+		result[version_, "pv_c"] <- wilcox.test(lastVersionData$classes, versionData$classes)$p.value;
+		result[version_, "pv_p"] <- wilcox.test(lastVersionData$packages, versionData$packages)$p.value;
 	}
-
-	vdata <- subset(data, version == version_);
-
-	accRevisions <- accRevisions + nrow(vdata);
-	accSingleRevisions <- accSingleRevisions + nrow(subset(vdata, classes == 1));
 	
-	classes <- subset(vdata)$classes;
-	accClasses <- c(accClasses, classes);
-	
-	packages <- subset(vdata)$packages;
-	accPackages <- c(accPackages, packages);
+	result[version_, "sw_p"] <- shapiro.test(versionData$classes)$p.value;		
+	lastVersion = version_;
 }
 
-result[lastVersion, "rev"] <- accRevisions;
-result[lastVersion, "single"] <- accSingleRevisions; 
-
-result[lastVersion, "revmc"] <- mean(accClasses);
-result[lastVersion, "revsc"] <- sd(accClasses);
-result[lastVersion, "pv_c"] <- wilcox.test(oldClasses, accClasses)$p.value; 
-
-result[lastVersion, "revmp"] <- mean(accPackages);
-result[lastVersion, "revsp"] <- sd(accPackages);
-result[lastVersion, "pv_p"] <- wilcox.test(oldPackages, accPackages)$p.value; 
-
 result
+
+boxplot(data$classes~data$version, outline=FALSE, cex.axis=0.75, main="Classes affected by commits (without outliers)")
+boxplot(data$classes~data$version, range=0, cex.axis=0.75, main="Classes affected by commits (with outliers)")
+
+boxplot(data$packages~data$version, outline=FALSE, cex.axis=0.75, main="Packages affected by commits (without outliers)")
+boxplot(data$packages~data$version, range=0, cex.axis=0.75, main="Packages affected by commits (with outliers)")
